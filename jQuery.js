8c8d6aaf52636d7bc2d574bc426f64c9d6d9a317
David-Zhang-test <1839476290@qq.com>
2020-04-01 19:54:06 +0800

    2

--- jQuery.js
+++ jQuery.js
@@ -2,3 +2,11 @@
 $(document).ready(function(){
 	$("#pp1").fadeIn(2000)
 });
+$(document).ready(function(){
+	$(".para1").mouseenter(function(){
+    	$(".para1").css("background-color","#141414");
+  	});
+	$(".para1").mouseleave(function(){
+    	$(".para1").css("background-color","black");
+  	});
+});
